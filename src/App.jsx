154f57c721f8172ecee608552ab655c85badc2bd
Yar Zar Nay Oo <yarzarnayoo27@gmail.com>
2026-02-04 15:25:58 +0630

    feat: implement responsive download section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Download from "./sections/Download";
 import Faq from "./sections/Faq";
 import Features from "./sections/Features";
 import Header from "./sections/Header";
@@ -14,6 +15,7 @@ const App = () => {
       <Pricing />
       <Faq />
       <Testimonials />
+      <Download />
     </main>
   );
 };
