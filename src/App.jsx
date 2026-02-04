f602cf11e50783c666c30b71efc0f11ec6a22af1
Yar Zar Nay Oo <yarzarnayoo27@gmail.com>
2026-02-04 16:12:34 +0630

    feat: implement responsive footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Download from "./sections/Download";
 import Faq from "./sections/Faq";
 import Features from "./sections/Features";
+import Footer from "./sections/Footer";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Pricing from "./sections/Pricing";
@@ -16,6 +17,7 @@ const App = () => {
       <Faq />
       <Testimonials />
       <Download />
+      <Footer />
     </main>
   );
 };
